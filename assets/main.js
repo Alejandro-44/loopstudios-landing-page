7d94bb96d377319bd88a003769afd2e0a1c41b3e
Alejandro <alejandroarguelloduarte@gmail.com>
2021-03-20 15:27:30 -0500

    ajustes al hero e interactividad para menu

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,13 @@
 const navToggle = document.getElementById('nav-toggle');
 const navMenu = document.querySelector('.nav__menu');
+const menuLink = document.querySelectorAll('.nav__link')
 
-navToggle.addEventListener('click', toggle);
+navToggle.addEventListener('click', toggleMenu);
+menuLink.forEach(n => n.addEventListener('click', linkAction))
 
-function toggle() {
+function toggleMenu() {
   navMenu.classList.toggle('active')
+}
+function linkAction() {
+  navMenu.classList.remove('active')
 }
