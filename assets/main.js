9cca5940cab52cae20f3f9a2be90d1c120d9959a
Alejandro <alejandroarguelloduarte@gmail.com>
2021-03-20 16:31:30 -0500

    añadiendo estados active

--- assets/main.js
+++ assets/main.js
@@ -9,5 +9,7 @@ function toggleMenu() {
   navMenu.classList.toggle('active')
 }
 function linkAction() {
-  navMenu.classList.remove('active')
+  menuLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active')
+  navMenu.classList.remove('active');
 }
