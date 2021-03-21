8efae918d28ffeeeb659b9aa304028a1f9fdbdb8
Alejandro <alejandroarguelloduarte@gmail.com>
2021-03-21 18:47:00 -0500

    simplificando variables

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,15 @@
 const navToggle = document.getElementById('nav-toggle');
-const navMenu = document.querySelector('.nav__menu');
+const menu = document.querySelector('.nav__menu');
 const menuLink = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', toggleMenu);
 menuLink.forEach(n => n.addEventListener('click', linkAction))
 
 function toggleMenu() {
-  navMenu.classList.toggle('active')
+  menu.classList.toggle('active')
 }
 function linkAction() {
   menuLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active')
-  navMenu.classList.remove('active');
+  menu.classList.remove('active');
 }
