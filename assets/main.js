aa8a5c8e4790de27396b83a887b36c33ef8def1a
Alejandro <alejandroarguelloduarte@gmail.com>
2021-03-21 20:37:39 -0500

    añadiendo boton de cerrrar para el menu

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,5 @@
 const navToggle = document.getElementById('nav-toggle');
+const burgerIcon = document.getElementById('burguer-icon');
 const menu = document.querySelector('.nav__menu');
 const menuLink = document.querySelectorAll('.nav__link')
 
@@ -6,8 +7,15 @@ navToggle.addEventListener('click', toggleMenu);
 menuLink.forEach(n => n.addEventListener('click', linkAction))
 
 function toggleMenu() {
-  menu.classList.toggle('active')
+  menu.classList.toggle('active');
+  if(menu.classList.contains('active')){
+    burgerIcon.setAttribute('src', './images/icon-close.svg')
+  } else {
+    burgerIcon.setAttribute('src', './images/icon-hamburger.svg')
+  }
+  
 }
+
 function linkAction() {
   menuLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active')
